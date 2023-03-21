56fc7e6da2845a78592071aa960e696498208154
54chair <takukoshi3721@gmail.com>
2023-03-21 06:31:35 +0000

    APIへのリクエストとレスポンスができるようにcontrollerとviewを編集

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -3,5 +3,18 @@ class ChatController < ApplicationController
   end
 
   def show
+    @keyword1 = params[:keyword1]
+    @keyword2 = params[:keyword2]
+    @keyword3 = params[:keyword3]
+    @keyword4 = params[:keyword4]
+    @keyword5 = params[:keyword5]
+
+    response = @client.chat(
+      parameters: {
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: "あなたはサービスの名前を付ける天才です。斬新で、わかりやすいサービス名を思いつくことが出来ます。#{@keyword1}、#{@keyword2}、#{@keyword3}、#{@keyword5}というキーワードから、サービス名を５個日本語で考えてみてください。なぜそう考えたのかも教えてください。" }],
+      })
+
+    @chats = response.dig("choices", 0, "message", "content")
   end
 end
